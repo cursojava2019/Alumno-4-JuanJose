1fc9a61ea9c4828c56270a929a849deee4793c01
JuanjoCobo <jjcobojimenez@gmail.com>
2019-01-14 11:00:55 +0000

    ejercicios jquery

--- ejerciciosJQuery/js/ejerciciosJQuery.js
+++ ejerciciosJQuery/js/ejerciciosJQuery.js
@@ -26,6 +26,74 @@
 
 
 		//1.3
+		//Obtener elementos ocultos
+		var elemOcultos =  $("*:hidden").length;
+		//Crear boton 
+		var botonOcultos = "<button id='botonOcultos'>Mostrar nº de elementos ocultos</button>";
+		//Obtener imagenes con alt y crear boton
+		var imagenesAlt = $("img[alt]").length;
+		var botonImgAlt = "<button id='botonImgAlt'>Mostrar nº de imagenes con Alt</button>";
+		//insertar botones en el index.html
+		$("label").before(botonOcultos, botonImgAlt);
+
+		$("#botonOcultos").click(function(){
+			alert(elemOcultos);
+		});
+		$("#botonImgAlt").click(function(){
+			alert(imagenesAlt);
+		});
+
+		//1.4
+		//Seleccionar filas impares de la table y ponerle fondo amarillo
+		$("tbody tr:even").css("background-color","yellow");
+
+	});
+
+	//EJERCICIO 2
+	$(document).ready(function(){
+
+		//2.1 
+		//Seleccionar img, mostrar su alt por consola y crear boton
+		var img = $("img");
+		var botonMostrarAlt = "<button id='botonMostrarAlt'>Mostrar alt img por consola</button>";
+		$("label").before(botonMostrarAlt);
+		$("#botonMostrarAlt").click(function(){
+			$(img).each(function(){
+				console.log($(this).attr('alt'));	
+			});
+		});
+
+		//2.2 Seleccionar input, poner fondo gris y crear boton
+		var botonInputGris = "<button id='botonInputGris'>Poner fondo gris</button>";
+		$("form:last").before(botonInputGris);
+		$("#botonInputGris").click(function(){
+			$('form:last').addClass("fondoGris");
+			$('.fondoGris').css("background-color","gray");
+		});
+
+		//2.3
+		//Seleccionar elemento con la clase 'current' dentro de #myList y eliminarla. Primero crear boton
+		var botonCurrent = "<button id='botonCurrent'>Cambiar clase 'current'</button>";
+		$("#myList").before(botonCurrent);
+		$("#botonCurrent").click(function(){
+			$("#myList .current").next().addClass("current");
+			$("#myList .bar").removeClass("current");
+		});
+		
+		//2.4
+		var botonSpecials= "<button id='botonSpecials'>Valor submit por consola</button>";
+		$("#specials").before(botonSpecials); 
+		$("#botonSpecials").click(function(){
+			console.log($('input[type="submit"]').attr('value'));
+		});
+
+		//2.5
+		var botonSlideShow = "<button id='botonSlideShow'>Boton SlideShow</button>";
+		$("#slideshow").before(botonSlideShow);
+		$("#botonSlideShow").click(function(){
+			$("#slideshow li:first").addClass("current");
+			$("#slideshow li:first").siblings('li').addClass("disabled");
+		});
 
 	});
 
