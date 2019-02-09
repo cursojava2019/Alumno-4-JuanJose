c6ed3c829f67b2c96508eb25a86247d39ace5320
cursojava <cursojava@192.168.1.94>
2019-02-09 12:54:57 +0100

    iniciando proyecto mvc jpa

---
+++ academiaMVCJPA/src/main/java/es/indra/academia/controller/profesores/ProfesorController.java
@@ -0,0 +1,139 @@
+package es.indra.academia.controller.profesores;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.indra.academia.authentication.MyUserDetails;
+import es.indra.academia.model.entities.Profesor;
+import es.indra.academia.model.service.ProfesorService;
+
+@Controller
+@RequestMapping("/admin/profesores")
+public class ProfesorController {
+	@Autowired
+	ProfesorService profesorService;
+
+	@Autowired
+	ProfesorFormValidator validador;
+	private Logger log = LogManager.getLogger(ProfesorController.class);
+
+	@RequestMapping(value = "/listado.html", method = RequestMethod.GET)
+	public String listado(Model model) {
+
+		MyUserDetails user = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String name = user.getUsername(); // get logged in username
+
+		this.log.info("listado Profesores");
+		List<Profesor> listado = this.profesorService.findAll();
+		model.addAttribute("listado", listado);
+		return "profesores/listado";
+	}
+
+	// Listado de profesores BUSCADOS por PATRÓN
+	@RequestMapping(value = "/listado.html", method = RequestMethod.POST)
+	public String listadoPatron(@RequestParam("patron") String patron, Model model) {
+
+		this.log.info("listado Profesores por patrón");
+		List<Profesor> listado = null;
+
+		if (patron != null || !patron.equals("")) {
+			listado = this.profesorService.findProfesoresPatron(patron);
+		} else {
+			listado = this.profesorService.findAll();
+		}
+
+		model.addAttribute("listado", listado);
+		return "profesores/listado";
+
+	}
+
+	@RequestMapping(value = "/nuevo.html", method = RequestMethod.GET)
+	public String nuevo(Model model) {
+		model.addAttribute("profesor", new ProfesorForm(new Profesor()));
+		return "profesores/nuevo";
+	}
+
+	@RequestMapping(value = "/nuevo.html", method = RequestMethod.POST)
+	public String nuevoPost(@Valid @ModelAttribute("profesor") ProfesorForm form, BindingResult result) {
+
+		this.validador.validate(form, result);
+		if (result.hasErrors()) {
+			return "profesores/nuevo";
+
+		} else {
+
+			this.profesorService.create(form.obtenerProfesor());
+			return "redirect:/admin/profesores/listado.html?mensaje=correcto";
+
+		}
+
+	}
+
+	@RequestMapping(value = "/modificar.html", method = RequestMethod.GET)
+	public String modificar(@RequestParam("id") Long id, Model model) {
+		if (id == null) {
+			return "redirect:/admin/profesores/listado.html?mensaje=errorId";
+
+		} else {
+			Profesor profesor = this.profesorService.find(id);
+			if (profesor != null) {
+				ProfesorForm form = new ProfesorForm(profesor);
+				model.addAttribute("formulario", form);
+				return "profesores/modificar";
+
+			} else {
+				return "redirect:/admin/profesores/listado.html?mensaje=errorId";
+			}
+
+		}
+
+	}
+
+	// CONSULTAR EL CREAR Y TERMINAR MODIFICAR
+
+	@RequestMapping(value = "/modificar.html", method = RequestMethod.POST)
+	public String modificarPost(@Valid @ModelAttribute("formulario") ProfesorForm form, BindingResult result) {
+
+		this.validador.validate(form, result);
+
+		if (result.hasErrors()) {
+			return "profesores/modificar";
+		} else {
+			this.profesorService.update(form.obtenerProfesor());
+			return "redirect:/admin/profesores/listado.html?mensaje=correcto";
+		}
+
+	}
+
+	@RequestMapping(value = "/eliminar.html", method = RequestMethod.GET)
+	public String eliminar(@RequestParam("id") Long id, Model model) {
+
+		if (id == null) {
+			return "redirect:/admin/profesores/listado.html?mensaje=errorId";
+		} else {
+			Profesor profesor = this.profesorService.find(id);
+			if (profesor != null) {
+				this.profesorService.delete(id);
+				return "redirect:/admin/profesores/listado.html?mensaje=correcto";
+			} else {
+				return "redirect:/admin/profesores/listado.html?mensaje=errorId";
+			}
+
+		}
+
+	}
+
+}
---
+++ academiaMVCJPA/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,22 @@
+
+
+	<div class="container">
+		
+		<div class="modal-dialog text-center">
+			<div class="main-section col-sm-8">
+				<div class="modal-content">
+				
+					<form class="col-10" action="./j_spring_security_check" method="post">
+						<div class="form-group">
+							<h2 class="text-white">Identifícate</h2>
+							<p><input class="form-control" name="j_username" type="text" placeholder="Usuario"></p>
+							<p><input class="form-control" name="j_password" type="password" placeholder="Contraseña"></p>
+							<button class="btn boton-login" type="submit">Acceder</button>
+						</div>
+					</form>
+				
+				</div>
+			</div>
+		</div>
+		
+	</div>
